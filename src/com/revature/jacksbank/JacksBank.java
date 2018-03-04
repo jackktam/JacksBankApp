99b01f7a17b6897c91ee4dbd9a3a62b9e1bc3937
jackktam <36857546+jackktam@users.noreply.github.com>
2018-03-03 22:45:53 -0500

    Basic shutdown functionality

--- src/com/revature/jacksbank/JacksBank.java
+++ src/com/revature/jacksbank/JacksBank.java
@@ -9,6 +9,7 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 import bankobjects.Account;
 import bankobjects.Admin;
@@ -28,7 +29,7 @@ public class JacksBank {
 	}
 	
 	static void start() {
-		
+		//if file not found or file empty, populates database with one admin and one employee before serializing to file and closing
 		if(!file.exists() || file.length()==0) {
 			try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName))) {
 				data = new BankDatabase();
@@ -42,6 +43,7 @@ public class JacksBank {
 			}
 		}	
 		
+		//deserialize database from file and stores it
 		try(ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName))) {   
 	        	data = (BankDatabase) in.readObject();
 	      } catch (IOException i) {
@@ -53,12 +55,15 @@ public class JacksBank {
 	         return;
 	      }
 		
+		//enters menu on completion of setup
 		menu();
 		
 	}
 	
 	public static void menu() {
-	
+		
+		shutDown();
+		
 	}
 
 	public static void register() {
@@ -69,8 +74,34 @@ public class JacksBank {
 		
 	}
 
-	public static boolean shutDown() {
+	public static void shutDown() {
 		
-		return false;
+		boolean finished = false;
+		
+		while(finished!=true) {
+			String valid = "";
+			System.out.println("Are you sure you want to shutdown the system? \nType in \"yes\" to exit and \"no\" to return to main menu:");
+			Scanner scanner = new Scanner(System.in);
+			valid = scanner.nextLine();
+			scanner.close();
+			switch(valid) {
+			case "yes": 
+				System.out.println("Thank you for using Jack's Bank, system exiting.");
+				try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName))) {
+					out.writeObject(data);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				finished = true;
+				break;
+			case "no":
+				System.out.println("Returning to main menu.");
+				menu();
+				break;
+			default:
+				System.out.println("Sorry invalid input, please try again.");
+			}
+		}
 	}
+	
 }
